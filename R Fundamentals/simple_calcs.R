2fd567c54b692ae94fa41a324ef79195a444a499
Ramb Memburg <46289413+memburg@users.noreply.github.com>
2020-11-07 12:47:13 -0600

    Add files via upload

---
+++ R Fundamentals/simple_calcs.R
@@ -0,0 +1,27 @@
+# Sum of variables
+office_time = 8
+studying = 1
+transport = 1.5
+day_time = office_time + studying + transport
+day_time
+
+# Getting grades
+quarter_1 = 0.3
+quarter_2 = 0.3
+quarter_3 = 0.4
+
+grade_1 = 4.0
+grade_2 = 4.6
+grade_3 = 3.0
+
+grade_q_1 = grade_1 * quarter_1
+grade_q_1
+
+grade_q_2 = grade_2 * quarter_2
+grade_q_2
+
+grade_q_3 = grade_3 * quarter_3
+grade_q_3
+
+final_grade = grade_q_1 + grade_q_2 + grade_q_3
+final_grade
